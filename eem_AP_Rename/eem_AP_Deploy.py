755aaf223ff430fc570c8ab76f03d20a4d6dd9b7
Gordon Rogier <grogier@cisco.com>
2026-08-26 13:41:52 -0500

    updates

--- eem_AP_Rename/eem_AP_Deploy.py
+++ eem_AP_Rename/eem_AP_Deploy.py
@@ -421,6 +421,7 @@ def get_speed_duplex(chk_ap=None):
     cli_ap = AccessPoint()
     pattern = defaultdict(lambda : re.compile(rf'~'))
     pattern['AP_NAME'] =            re.compile(rf"^(AP Name\s+:|Ethernet Stats for AP)\s+(\S+)")
+    pattern['AP_NAME'] =            re.compile(rf"^(?:AP Name\s+:|Ethernet Stats for AP)\s+(\S+)")
     # Ethernet Stats for AP BLAH
     pattern['AP_SPEED_DUPLEX'] =    re.compile(rf"^(GigabitEthernet\d)\s+(\S+)\s+(\d+)\s+(Mbps)\s+(\S+)")
     cli_match = defaultdict(lambda : re.search(pattern['~'],'BLANK'))
