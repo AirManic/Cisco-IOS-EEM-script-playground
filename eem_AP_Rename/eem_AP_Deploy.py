72923c25b403812e9798a248a9b91143fb22ea0a
Gordon Rogier <grogier@cisco.com>
2026-08-29 17:06:17 -0500

    updates

--- eem_AP_Rename/eem_AP_Deploy.py
+++ eem_AP_Rename/eem_AP_Deploy.py
@@ -324,11 +324,20 @@ def get_ap_cdp(chk_ap=None):
     global ONLINE_APs
     if chk_ap is None: return
     if args_global.name is not None and args_global.name != "ALL":
-        # TODO fix sleep
+        wait_time = 211
         send_ios_syslog(severity=l_INFO,
-                        message=f"{args_global.name} single AP sleeping 210 sec to wait for CDP information")
+                        message=f"{args_global.name} waiting up to {wait_time} seconds for CDP information")
+        start_time = time.time()
         if is_guestshell:
-            time.sleep(210.001)  # Allow time for AP CDP to roll in.. take about 3 1/2 mins
+            while len(cli_results['show_cdp_neighbor']) < 10 and time.time() - start_time < wait_time:
+                cli_results['show_cdp_neighbor'] = show_ap(command=f"show ap name {chk_ap['AP_NAME']} cdp neighbor detail")
+        send_ios_syslog(severity=l_INFO,
+                        message=f"{args_global.name} got CDP information after {time.time() - start_time} seconds")
+        # # TODO fix sleep
+        # send_ios_syslog(severity=l_INFO,
+        #                     message=f"{args_global.name} single AP sleeping 210 sec to wait for CDP information")
+        # if is_guestshell:
+        #     time.sleep(210.001)  # Allow time for AP CDP to roll in.. take about 3 1/2 mins
     cli_results['show_cdp_neighbor'] = show_ap(command=f"show ap name {chk_ap['AP_NAME']} cdp neighbor detail")
     if not is_guestshell:
         cli_results['show_cdp_neighbor'] = fetch_file(file=SIM_FILE_EEM_AP_CDP_DETAIL)
